4275a062ebc97232dce57b3d7555da03c91d07ab
mohebul123 <mohebulhassan123@gmail.com>
2024-02-04 23:02:34 +0600

    Bubble sort completed

--- Sorting/bubble-sort.cpp
+++ Sorting/bubble-sort.cpp
@@ -1,7 +1,20 @@
 #include<stdio.h>
-
+#include<iostream>
+using namespace std;
 int main()
-{
-    printf("Hello");
+{   
+    int ar[5]={12,2,3,10,4};
+    int i,j;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<=5-2;j++){
+            if(ar[j]>ar[j+1]){
+                swap(ar[j],ar[j+1]);
+            }
+        }
+    }
+    for(j=0;j<5;j++)
+    {
+        printf("%d ",ar[j]);
+    }
     return 0;
 }
